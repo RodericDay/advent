ec233b02e6df489e4e15f483876305cb3e4125ba
Roderic Day <roderic.day@gmail.com>
2016-12-04 18:56:09 -0500

    More OOP

--- 2015/22.py
+++ 2015/22.py
@@ -1,6 +1,23 @@
 import re, collections, types
 
-GameState = collections.namedtuple("GameState", "player_hp player_mp enemy_hp enemy_atk ongoing")
+class GameState:
+
+    def __init__(self, player_hp, player_mp, enemy_hp, enemy_atk, ongoing):
+        self.player_hp = player_hp
+        self.player_mp = player_mp
+        self.enemy_hp = enemy_hp
+        self.enemy_atk = enemy_atk
+        self.ongoing = ongoing
+
+    def copy(self):
+        return GameState(**self.__dict__)
+
+    def __str__(self):
+        inner = ", ".join("{}={}".format(k, getattr(self, k))
+            for k in ["player_hp", "enemy_hp"])
+        return "GameState({})".format(inner)
+
+
 Spell = collections.namedtuple("Spell", "cost duration")
 spell_book = {
     "Magic Missile": Spell(53, 0),
@@ -10,44 +27,45 @@ spell_book = {
     "Recharge": Spell(229, 5),
 }
 
+
 def resolve(spell_cast, state, hard=False):
-    player_hp, player_mp, enemy_hp, enemy_atk, ongoing = state
+    state = state.copy()
     player_turn = spell_cast is not None
 
     if hard and player_turn:
-        player_hp -= 1
-        if player_hp < 1: return "loss", None
+        state.player_hp -= 1
+        if state.player_hp < 1: return "loss", None
 
     # resolve ongoing effects
-    ongoing_spells = collections.Counter(ongoing)
+    ongoing_spells = collections.Counter(state.ongoing)
     active = set(ongoing_spells.elements())
     ongoing_spells -= {k:1 for k in active}  # decrease cooldown
     player_def = 7 if 'Shield' in active else 0
-    player_mp += 101 if 'Recharge' in active else 0
-    enemy_hp -= 3 if 'Poison' in active else 0
-    if enemy_hp < 1: return "win", None
+    state.player_mp += 101 if 'Recharge' in active else 0
+    state.enemy_hp -= 3 if 'Poison' in active else 0
+    if state.enemy_hp < 1: return "win", None
 
     if player_turn:
         # casting an active spell is invalid
         if spell_cast in ongoing_spells.elements(): return "invalid", None
         # casting a spell without required mp is invalid
-        player_mp -= spell_book[spell_cast].cost
-        if player_mp < 0: return "invalid", None
+        state.player_mp -= spell_book[spell_cast].cost
+        if state.player_mp < 0: return "invalid", None
         # assign cooldown
         ongoing_spells[spell_cast] = spell_book[spell_cast].duration
         # handle instantaneous spells
-        if spell_cast == 'Magic Missile': enemy_hp -= 4
-        if spell_cast == 'Drain': enemy_hp -= 2; player_hp += 2
+        if spell_cast == 'Magic Missile': state.enemy_hp -= 4
+        if spell_cast == 'Drain': state.enemy_hp -= 2; state.player_hp += 2
         # win condition
-        if enemy_hp < 1: return "win", None
+        if state.enemy_hp < 1: return "win", None
 
     else:
-        player_hp -= max(1, enemy_atk-player_def)
-        if player_hp < 1: return "loss", None
+        state.player_hp -= max(1, state.enemy_atk-player_def)
+        if state.player_hp < 1: return "loss", None
 
     # state transfer
-    ongoing = tuple(ongoing_spells.elements())
-    return "ongoing", GameState(player_hp, player_mp, enemy_hp, enemy_atk, ongoing)
+    state.ongoing = tuple(ongoing_spells.elements())
+    return "ongoing", state
 
 def resolve_sequence(state, sequence):
     for spell in (action for spell in sequence for action in [spell, None]):
