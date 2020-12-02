9aae9a8ec069c285874ff53a7ad6cb23eefe87e7
Roderic Day <roderic.day@gmail.com>
2020-12-02 00:10:25 -0500

    2020/2

--- y2020/p02.py
+++ y2020/p02.py
@@ -1,4 +1,13 @@
+import re
 import sys
 
 
-sys.stdin.read().splitlines()
+ans1 = 0
+ans2 = 0
+for line in sys.stdin.read().splitlines():
+    lo, hi, char, pw = re.findall(r'(\d+)-(\d+) (\w): (\w+)', line)[0]
+    lo, hi = int(lo), int(hi)
+    ans1 += lo <= pw.count(char) <= hi
+    ans2 += sum([pw[lo - 1] == char, pw[hi - 1] == char]) == 1
+print(ans1)
+print(ans2)
